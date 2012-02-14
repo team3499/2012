4309cb5528ab47fc04e6430e93b0553cdcd6476d
Brian Turnbull <post@brianturnbull.com>
2012-02-14 17:58:29 -0500

    Separate Shooter and Magazine in separate subsystems

---
+++ Subsystems/Magazine.cpp
@@ -0,0 +1,48 @@
+#include "Subsystems/Magazine.h"
+#include "Robotmap.h"
+
+Magazine::Magazine() : Subsystem("Magazine") {
+  beltLeft  = new Victor(MAGAZINE_BELT_LEFT_CHANNEL);
+  beltRight = new Victor(MAGAZINE_BELT_RIGHT_CHANNEL);
+
+  buttonFront  = new DigitalIOButton(MAGAZINE_BUTTON_FRONT_CHANNEL);
+  buttonMiddle = new DigitalIOButton(MAGAZINE_BUTTON_MIDDLE_CHANNEL);
+  buttonBack   = new DigitalIOButton(MAGAZINE_BUTTON_BACK_CHANNEL);
+}
+
+void Magazine::InitDefaultCommand() {
+  // SetDefaultCommand(new MySpecialCommand());
+}
+
+//
+// Returns true if a ball is present in the front position
+// of the magazine
+//
+bool Magazine::IsReadyToFire() {
+  return buttonFront->Get();
+}
+
+//
+// Returns the number of balls loaded in the magazine
+//
+int Magazine::BallCount() {
+  int count = 0;
+
+  LoadStatus status = GetLoadStatus();
+  if (status.front) { count++; }
+  if (status.middle) { count++; }
+  if (status.back) { count++; }
+
+  return count;
+}
+
+//
+// Returns a LoadStatus struct with the load status
+// of each magazine position as a boolean
+//
+Magazine::LoadStatus Magazine::GetLoadStatus() {
+  LoadStatus status = { buttonFront->Get(),
+                        buttonMiddle->Get(),
+                        buttonBack->Get() };
+  return status;
+}
--- Subsystems/Shooter.h
+++ Subsystems/Shooter.h
@@ -7,26 +7,12 @@
 class Shooter : public Subsystem {
 
 private:
-  Victor *          beltTop;
-  Victor *          beltBottom;
-  DigitalIOButton * buttonMagazineFront;
-  DigitalIOButton * buttonMagazineMiddle;
-  DigitalIOButton * buttonMagazineBack;
+  Victor * beltTop;
+  Victor * beltBottom;
 
 public:
-  struct MagazineStatus {
-    bool front;
-    bool middle;
-    bool back;
-  };
-
   Shooter();
   void InitDefaultCommand();
-
-  bool IsReadyToFire();               // Is a ball in the front magazine position
-  int BallsInMagazine();              // Number of balls loaded in magazine (0 - 3)
-  MagazineStatus GetMagazineStatus(); // Load status of magazine
-
 };
 
 #endif /* __SUBSYSTEM_SHOOTER_H__ */
