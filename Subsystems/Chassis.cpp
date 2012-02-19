8b18859188bf895779d01374b7e0a29de2ed7f3c
Brian Turnbull <post@brianturnbull.com>
2012-02-19 12:49:36 -0500

    Instrument chassis

--- Subsystems/Chassis.cpp
+++ Subsystems/Chassis.cpp
@@ -1,6 +1,7 @@
 #include "Chassis.h"
 #include "Robotmap.h"
 #include "Commands/UserDrive.h"
+#include "SmartDashboard/SmartDashboard.h"
 
 Chassis::Chassis() : Subsystem("Chassis"),
 	left(DRIVE_LEFT),
@@ -10,16 +11,21 @@ Chassis::Chassis() : Subsystem("Chassis"),
     
 void Chassis::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
+  SmartDashboard::Log("Default", "Chassis Control");
 	SetDefaultCommand(new UserDrive());
 }
 
 void Chassis::ArcadeDrive(Joystick &joystick){
+  SmartDashboard::Log("Arcade", "Chassis Control");
 	Drive(joystick.GetY(),joystick.GetX());
 }
 
 void Chassis::TankDrive(float leftOut, float rightOut){
 	if(leftOut > 1.0){leftOut = 1.0;} else if(leftOut < -1.0){leftOut = -1.0;}
 	if(rightOut > 1.0){rightOut = 1.0;} else if(rightOut < -1.0){rightOut = -1.0;}
+  SmartDashboard::Log("Tank", "Chassis Control");
+  SmartDashboard::Log(leftOut, "Chassis Left Motor");
+  SmartDashboard::Log(rightOut, "Chassis Right Motor");
 	left.Set(leftOut, 0x80);
 	right.Set(rightOut, 0x80);
 }
@@ -58,6 +64,8 @@ void Chassis::Drive(float moveValue, float rotateValue)
 	}
 	if(leftOut > 1.0){leftOut = 1.0;} else if(leftOut < -1.0){leftOut = -1.0;}
 	if(rightOut > 1.0){rightOut = 1.0;} else if(rightOut < -1.0){rightOut = -1.0;}
+  SmartDashboard::Log(leftOut, "Chassis Left Motor");
+  SmartDashboard::Log(rightOut, "Chassis Right Motor");
 	left.Set(leftOut, 0x80);
 	right.Set(-rightOut, 0x80);
 }
