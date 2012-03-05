d9948534b8f16794040b96b96414debec58a809c
Brian Turnbull <post@brianturnbull.com>
2012-03-04 19:53:59 -0500

    Iterative ArmMove with quieting

--- Commands/ArmMove.cpp
+++ Commands/ArmMove.cpp
@@ -1,46 +1,73 @@
 #include "ArmMove.h"
 #include "WPILib.h"
 
-ArmMove::ArmMove(float angle)
+ArmMove::ArmMove(float angle, float margin)
 {
-  // Use requires() here to declare subsystem dependencies
   Requires(arm);
   Requires(accelerometer);
 
-  targetAngle = angle;
+  iteration    = 0;
+  targetAngle  = angle;
+  this->margin = margin;
 }
 
-// Called just before this Command runs the first time
+// Initialize will set the iteration counter to zero, wait a bit,
+// measure the current arm angle, and then start the motors moving
+// in the right direction.
 void ArmMove::Initialize() {
+  iteration = 0;
+  MeasureAndMove();
 }
 
-// Called repeatedly when this Command is scheduled to run
+
+// Checks if we've reached the target angle.  If so, stop, wait a bit
+// and then check if we are still there.  If not, start moving again.
 void ArmMove::Execute() {
-  float angle = accelerometer->GetArmDegree();
-  if((angle < targetAngle && angle > -75)){
-    arm->Move(true);
-  }else if(angle > targetAngle && angle < 90 ){
-    arm->Move(true);
-  } else {
-    arm->Stop();
+  if (IsAtTargetAngle()) {
+    ++iteration;
+    StopAndQuiet();
+    if (!IsAtTargetAngle()) { MeasureAndMove(); }
   }
 }
 
-// Make this return true when this Command no longer needs to run execute()
+// Bail out once we are at the target angle or if we've tried 3 times
+// TODO - is 3 times enough?
 bool ArmMove::IsFinished() {
-  float angle = accelerometer->GetArmDegree();
-  if (angle > targetAngle - 2 && angle < targetAngle + 2){
-    return true;
-  }
-  return false;
+  return (IsAtTargetAngle() || iteration > 3 );  
 }
 
-// Called once after isFinished returns true
 void ArmMove::End() {
-  
+  arm->Stop();
 }
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
 void ArmMove::Interrupted() {
+  End();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Stop the arm and wait for it to cease bouncing, take an angle measurement,
+// and then start moving again.
+void ArmMove::MeasureAndMove() {
+  StopAndQuiet();
+  if (!IsAtTargetAngle()) { arm->Move(GetDesiredDirection()); }
+}
+
+// Stop the arm and wait for it to cease bouncing
+void ArmMove::StopAndQuiet() {
+  arm->Stop();
+  Wait(0.5);
+}
+
+// Returns true if the arm needs to move forward, false if backward
+// (see Subsystems/Arm.cpp)
+bool ArmMove::GetDesiredDirection() {
+  return (accelerometer->GetArmDegree() > targetAngle);
+}
+
+// Returns true if the arm is close to the desired target angle or if
+// it's going to hit the robot
+bool ArmMove::IsAtTargetAngle() {
+  float currentAngle = accelerometer->GetArmDegree();
+  return (currentAngle < -75 || currentAngle > 90.0 || (currentAngle + margin > targetAngle && currentAngle - margin < targetAngle));
 }
